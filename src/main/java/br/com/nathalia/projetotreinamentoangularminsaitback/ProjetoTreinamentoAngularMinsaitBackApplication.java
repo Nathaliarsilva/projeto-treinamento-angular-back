6219e8c253844cde9fccaf0c0e37fafe161cbeae
Nathalia Ramos <nathii.siilva@gmail.com>
2023-11-02 21:01:01 -0300

    Iniciando projeto e swagger.

---
+++ src/main/java/br/com/nathalia/projetotreinamentoangularminsaitback/ProjetoTreinamentoAngularMinsaitBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.nathalia.projetotreinamentoangularminsaitback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTreinamentoAngularMinsaitBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTreinamentoAngularMinsaitBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nathalia/projetotreinamentoangularminsaitback/ProjetoTreinamentoAngularMinsaitBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nathalia.projetotreinamentoangularminsaitback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoTreinamentoAngularMinsaitBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
